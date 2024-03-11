6fc7a4d076a452013fc0b16ea0e8224233fcea77
zzVu77 <nguyenvanvu24062004@gmail.com>
2024-03-11 23:01:45 +0700

    feature: Add PurchasingInterface, Item_View, Item_DetailsInfo

---
+++ UTEMerchant/TextToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UTEMerchant
+{
+    public class TextToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = value as string;
+            return string.IsNullOrEmpty(text) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
