a16530a43223ef93e96fa46d1c604afffd95699c
kiyotaka1102 <phantrieuhuy2004@gmail.com>
2024-03-12 16:02:23 +0700

    feat: add login UI

---
+++ UTEMerchant/WinLogin.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UTEMerchant
+{
+    /// <summary>
+    /// Interaction logic for WinLogin.xaml
+    /// </summary>
+    public partial class WinLogin : Window
+    {
+        public WinLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUserName.Text) && txtUserName.Text.Length > 0) 
+            {
+                textUserName.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textUserName.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPassword.Text) && txtPassword.Text.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textUserName_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtUserName.Focus();
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtUserName.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Text) && !string.IsNullOrEmpty(txtUserName.Text))
+            {
+                MessageBox.Show("successfully signed up!!!!!");
+            }
+
+        }
+
+        private void textForgot_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void textRegister_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
