44a8e231ad2c57eff73b0c870577383651ff0110
SAPAAAA <hoanglanbui1967@gmail.com>
2024-03-20 11:40:38 +0700

    fix: resizing image in shopping cart feat: add property and auto input data

--- UTEMerchant/UC_ShoppingCartItemView.xaml.cs
+++ UTEMerchant/UC_ShoppingCartItemView.xaml.cs
@@ -20,9 +20,22 @@ namespace UTEMerchant
     /// </summary>
     public partial class UC_ShoppingCartItemView : UserControl
     {
+        private Item shoppingCartItem;
+
         public UC_ShoppingCartItemView()
         {
             InitializeComponent();
+        }
+
+        public UC_ShoppingCartItemView(Item item) : this() 
+        {
+            user_DAO user_DAO = new user_DAO();
+            user_DAO.Load();
+            shoppingCartItem = item;
+            tbShopName.Text = user_DAO.Users.Where(u => u.Id_user == item.user_id).FirstOrDefault().Name_shop;
+            tbItemName.Text = item.Name;
+            tbItemPrice.Text = $"{item.Price.ToString()}$";
+            
         }
 
         private void tbShopName_MouseDown(object sender, MouseButtonEventArgs e)
