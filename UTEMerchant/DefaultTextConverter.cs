8bafea9b725016f78067fa44d2ac51f2505fff3f
SAPAAAA <hoanglanbui1967@gmail.com>
2024-03-26 19:47:29 +0700

    feat: add converters

---
+++ UTEMerchant/DefaultTextConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UTEMerchant
+{
+    public class DefaultTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string defaultText = parameter as string;
+            string text = value as string;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return defaultText;
+            }
+            else
+            {
+                return text;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
