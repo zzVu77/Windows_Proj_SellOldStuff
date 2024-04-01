283264c4d11d673b02048c1220c7f071e7b8a874
SAPAAAA <hoanglanbui1967@gmail.com>
2024-04-01 16:02:15 +0700

    fix: notice the registration is complete

--- UTEMerchant/UC_RegistrationComplete.xaml.cs
+++ UTEMerchant/UC_RegistrationComplete.xaml.cs
@@ -24,5 +24,10 @@ namespace UTEMerchant
         {
             InitializeComponent();
         }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
