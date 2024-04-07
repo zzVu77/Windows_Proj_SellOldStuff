3b481f96afbaf3ea2f03c90b3d402fb40a2925f7
SAPAAAA <hoanglanbui1967@gmail.com>
2024-04-07 22:14:35 +0700

    feat: Binding data

--- UTEMerchant/UC_ToReceiveItem.xaml.cs
+++ UTEMerchant/UC_ToReceiveItem.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,31 @@ namespace UTEMerchant
     /// </summary>
     public partial class UC_ToReceiveItem : UserControl
     {
+
+        private readonly Item Item;
+        private readonly User User;
+
         public UC_ToReceiveItem()
         {
             InitializeComponent();
+        }
+
+        public UC_ToReceiveItem(Item item, User user) : this()
+        {
+            this.Item = item;
+            this.User = user;
+            SetData(item, user);
+        }
+
+        //Binding data to the UI
+        private void SetData(Item item, User user)
+        {
+            imgToReceiveItem.Source = item.Image;
+            txblShopName.Text = user.Name;
+            txblToReceiveOriginalPrice.Text = $"{item.Original_Price.ToString(CultureInfo.InvariantCulture)}$";
+            txblToReceivePrice.Text = $"{item.Price.ToString(CultureInfo.InvariantCulture)}$";
+            txblToReceiveConditon.Text = $"{item.Condition.ToString(CultureInfo.InvariantCulture)}%";
+            txblToReceiveItemName.Text = item.Name;
         }
     }
 }
