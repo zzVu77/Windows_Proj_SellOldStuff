76861760a0dd2786688528d1f0c08c38e00868a9
kiyotaka1102 <phantrieuhuy2004@gmail.com>
2024-04-14 14:02:32 +0700

    feat:add table address

---
+++ UTEMerchant/Address_DAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace UTEMerchant
+{
+    internal class Address_DAO : DAO<Address>
+    {
+        public Address_DAO() { }
+        public override List<Address> Load() // More descriptive method name
+        {
+            return db.LoadData<Address>("SELECT * FROM [dbo].[Address]");
+        }
+        public List<string> getdistrict(string city)
+        {
+            
+            return db.LoadData<Address>("SELECT * FROM [dbo].[Address]").Where(a => a.City == city)
+                 .Select(a => a.District)
+                 .Distinct().ToList();
+        }
+    }
+}
--- UTEMerchant/WinBuyingInterface.xaml.cs
+++ UTEMerchant/WinBuyingInterface.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,6 +25,7 @@ namespace UTEMerchant
         public Item info;
         List<User> users = new List<User>();
         user_DAO user_dao = new user_DAO();
+        Address_DAO address_dao = new Address_DAO();
         private int Id_User;
         public WinBuyingInterface()
         {
@@ -47,6 +49,12 @@ namespace UTEMerchant
             txblTotalValue.Text = info.Price.ToString() + "$";
             var resourceUri = new Uri(info.Image_Path, UriKind.RelativeOrAbsolute);
             imgOrderItem.Source = new BitmapImage(resourceUri);
+            List<Address> distinctCities = address_dao.Load();
+            cbPickupCity.Items.Clear();
+            foreach (Address address in distinctCities)
+            {
+                cbPickupCity.Items.Add(address.City);
+            }
 
         }
 
@@ -57,7 +65,28 @@ namespace UTEMerchant
             item.Item_Id = info.Item_Id;
             new Item_DAO().UpdateStatus(item.Item_Id);
             new PurchasedItem_DAO().AddItem(item);
+
             this.Close();
+        }
+
+        private void cbPickupCity_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbPickupCity.SelectedItem != null)
+            {
+                string selectedCity = cbPickupCity.SelectedItem.ToString();
+
+                // Filter districts based on selected city
+                var filteredDistricts = address_dao.getdistrict(selectedCity);
+
+                // Update district ComboBox
+                if (cbPickupCity.Items != null)
+                    cbPickupDistrict.Items.Clear();
+                foreach (var district in filteredDistricts)
+                {
+                    cbPickupDistrict.Items.Add(district);
+                }
+                cbPickupDistrict.SelectedIndex = -1;
+            }
         }
     }
 }
