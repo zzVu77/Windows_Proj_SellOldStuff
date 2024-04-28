cf46f1bc90c699d896a061abaa6c5ba4628abf55
SAPAAAA <hoanglanbui1967@gmail.com>
2024-04-28 14:45:27 +0700

    feat: make delivering view in boxes

---
+++ UTEMerchant/UC_DeliveringItem.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UTEMerchant
+{
+    /// <summary>
+    /// Interaction logic for UC_DeliveringItem.xaml
+    /// </summary>
+    public partial class UC_DeliveringItem : UserControl
+    {
+        private readonly Item _item;
+
+        public UC_DeliveringItem()
+        {
+            InitializeComponent();
+        }
+
+        public UC_DeliveringItem(Item item) : this()
+        {
+            this._item = item;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_item != null)
+            {
+                tbName.Text = _item.Name;
+                tbDiscountedPrice.Text = _item.Price.ToString("C", CultureInfo.CurrentCulture);
+                tbOriginalPrice.Text = _item.Original_Price.ToString("C", CultureInfo.CurrentCulture);
+                tbTotalValue.Text = _item.Price.ToString("C", CultureInfo.CurrentCulture) + "$";
+                //var resourceUri = new Uri(_item.Image_Path, UriKind.RelativeOrAbsolute);
+                //imgItem.Source = new BitmapImage(resourceUri);
+            }
+        }
+
+        public Item Item => _item;
+    }
+}
--- UTEMerchant/UC_DeliveringItemsBox.xaml.cs
+++ UTEMerchant/UC_DeliveringItemsBox.xaml.cs
@@ -22,42 +22,59 @@ namespace UTEMerchant
     public partial class UC_DeliveringItemsBox : UserControl
     {
         public event EventHandler ReceivedButtonClicked;
-        private readonly Item Item;
-        private readonly Seller SellerOfItem;
-        private PurchasedItem_DAO purchasedItemDAO = new PurchasedItem_DAO();
-        private int userID;        
+        private readonly List<Item> _items;
+        private readonly Seller _seller;
+        private readonly int _userId;        
         public UC_DeliveringItemsBox()
         {
             InitializeComponent();
             this.Width = 1300;
         }
 
-        public UC_DeliveringItemsBox(Item item, Seller seller, int userID) : this()
+        public UC_DeliveringItemsBox(List<Item> items, Seller seller, int userId) : this()
         {
-            this.Item = item;
-            this.userID=userID;
-            this.SellerOfItem = seller;
-            SetData(item, SellerOfItem);
+            this._items = items;
+            this._userId = userId; 
+            this._seller = seller;
         }
 
-        //Binding data to the UI
-        private void SetData(Item item, Seller seller)
-        {
-            var resourceUri = new Uri(item.Image_Path, UriKind.RelativeOrAbsolute);
-            imgToReceiveItem.Source = new BitmapImage(resourceUri);
-            txblShopName.Text = SellerOfItem.ShopName;
-            txblToReceiveOriginalPrice.Text = $"{Item.Original_Price.ToString(CultureInfo.InvariantCulture)}$";
-            txblToReceivePrice.Text = $"{Item.Price.ToString(CultureInfo.InvariantCulture)}$";
-            txblToReceiveConditon.Text = $"{Item.Condition.ToString(CultureInfo.InvariantCulture)}%";
-            txblToReceiveItemName.Text = Item.Name;
-            txblToReceiveTotalValue.Text = Item.Price.ToString();
-        }
-
-        private void btnToReceiveReceived_Click(object sender, RoutedEventArgs e)
+        private void btnReceived_Click(object sender, RoutedEventArgs e)
         {
             //string deliveryStatus = purchasedItemDAO.GetPurchasedProductStatus(Item.Item_Id, this.userID);
-            purchasedItemDAO.UpdateDeliveryStatus(Item.Item_Id, this.userID, "delivered");
+            foreach (var item in _items)
+            {
+                new PurchasedItem_DAO().UpdateDeliveryStatus(item.Item_Id, this._userId, "delivered");
+            }
             ReceivedButtonClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_seller != null)
+            {
+                tbShopName.Text = _seller.ShopName;
+            }
+
+            if (_items != null)
+            {
+                foreach (var item in _items)
+                {
+                    AddItem(item);
+                }
+            }
+        }
+
+        private void AddItem(Item item)
+        {
+            foreach (UC_DeliveringItem ucDeliveringItem in spItems.Children)
+            {
+                if (ucDeliveringItem.Item.Item_Id == item.Item_Id)
+                {
+                    return;
+                }
+            }
+            UC_DeliveringItem uc_item = new UC_DeliveringItem(item);
+            spItems.Children.Add(uc_item);
         }
     }
 }
